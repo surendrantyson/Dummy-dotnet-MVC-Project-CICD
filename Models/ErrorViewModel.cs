e9e8f0e2faf4bac9af9170c0a8a7248a5f05a56c
surendrantyson <surendrantyson.c@gmail.com>
2024-03-01 19:31:10 +0530

    project cretaed

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Dummy_dotnet_MVC_Project_CICD.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
